19a49dcafaeee2997824c3fbb2de053b827f14a7
Tsapko <ceklon@gmail.com>
2019-12-06 03:17:01 +0200

    ver-5-start

--- js/main.js
+++ js/main.js
@@ -1,67 +1,131 @@
-// var slideIndex1 = 1;
-// showDivs1(slideIndex1);
+// particles size - line 23 - value 1 smal particl 10 big
+var canvas = document.querySelector("#scene"),
+ctx = canvas.getContext("2d"),
+particles = [],
+amount = 0,
+mouse = {x:0,y:0},
+radius = 1;
 
-// function slideBtn1(n) {
-//     showDivs1(slideIndex1 += n);
-// }
-// function showDivs1(n) {
-//     var i;
-//     var x = document.getElementsByClassName("slides1");
-//     if (n > x.length) {slideIndex1 = 1}    
-//     if (n < 1) {slideIndex1 = x.length}
-//     for (i = 0; i < x.length; i++) {
-//         x[i].style.display = "none";  
-//     }
-//     x[slideIndex1-1].style.display = "flex";
-// }
+var colors = ["#468966","#FFF0A5", "#FFB03B","#B64926", "#8E2800"];
 
-// var slideIndex2 = 1;
-// showDivs2(slideIndex2);
+var copy = document.querySelector("#copy");
 
-// function slideBtn2(n) {
-//     showDivs2(slideIndex2 += n);
-// }
-// function showDivs2(n) {
-//     var i;
-//     var x = document.getElementsByClassName("slides2");
-//     if (n > x.length) {slideIndex2 = 1}    
-//     if (n < 1) {slideIndex2 = x.length}
-//     for (i = 0; i < x.length; i++) {
-//         x[i].style.display = "none";  
-//     }
-//     x[slideIndex2-1].style.display = "flex";
-// }
+var ww = canvas.width = window.innerWidth;
+var wh = canvas.height = window.innerHeight;
 
-// var slideIndex3 = 1;
-// showDivs3(slideIndex3);
+function Particle(x,y){
+this.x =  Math.random()*ww;
+this.y =  Math.random()*wh;
+this.dest = {
+    x : x,
+    y: y
+};
+// particles size
+this.r =  Math.random()*0.1 + 20;
+this.vx = (Math.random()-0.5)*20;
+this.vy = (Math.random()-0.5)*20;
+this.accX = 0;
+this.accY = 0;
+this.friction = Math.random()*0.05 + 0.94;
 
-// function slideBtn3(n) {
-//     showDivs3(slideIndex3 += n);
-// }
-// function showDivs3(n) {
-//     var i;
-//     var x = document.getElementsByClassName("slides3");
-//     if (n > x.length) {slideIndex3 = 1}    
-//     if (n < 1) {slideIndex3 = x.length}
-//     for (i = 0; i < x.length; i++) {
-//         x[i].style.display = "none";  
-//     }
-//     x[slideIndex3-1].style.display = "flex";
-// }
+this.color = colors[Math.floor(Math.random()*6)];
+}
 
-// var slideIndex4 = 1;
-// showDivs4(slideIndex4);
+Particle.prototype.render = function() {
 
-// function slideBtn4(n) {
-//     showDivs4(slideIndex4 += n);
-// }
-// function showDivs4(n) {
-//     var i;
-//     var x = document.getElementsByClassName("slides4");
-//     if (n > x.length) {slideIndex4 = 1}    
-//     if (n < 1) {slideIndex4 = x.length}
-//     for (i = 0; i < x.length; i++) {
-//         x[i].style.display = "none";  
-//     }
-//     x[slideIndex4-1].style.display = "flex";
-// }
+
+this.accX = (this.dest.x - this.x)/1000;
+this.accY = (this.dest.y - this.y)/1000;
+this.vx += this.accX;
+this.vy += this.accY;
+this.vx *= this.friction;
+this.vy *= this.friction;
+
+this.x += this.vx;
+this.y +=  this.vy;
+
+ctx.fillStyle = this.color;
+ctx.beginPath();
+ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
+ctx.fill();
+
+var a = this.x - mouse.x;
+var b = this.y - mouse.y;
+
+var distance = Math.sqrt( a*a + b*b );
+if(distance<(radius*70)){
+    this.accX = (this.x - mouse.x)/100;
+    this.accY = (this.y - mouse.y)/100;
+    this.vx += this.accX;
+    this.vy += this.accY;
+}
+
+}
+
+function onMouseMove(e){
+mouse.x = e.clientX;
+mouse.y = e.clientY;
+}
+
+function onTouchMove(e){
+if(e.touches.length > 0 ){
+mouse.x = e.touches[0].clientX;
+mouse.y = e.touches[0].clientY;
+}
+}
+
+function onTouchEnd(e){
+mouse.x = -9999;
+mouse.y = -9999;
+}
+
+function initScene(){
+ww = canvas.width = window.innerWidth;
+wh = canvas.height = window.innerHeight;
+
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+ctx.font = "bold "+(ww/10)+"px sans-serif";
+ctx.textAlign = "center";
+ctx.fillText(copy.value, ww/2, wh/2);
+
+var data  = ctx.getImageData(0, 0, ww, wh).data;
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.globalCompositeOperation = "screen";
+
+particles = [];
+for(var i=0;i<ww;i+=Math.round(ww/150)){
+    for(var j=0;j<wh;j+=Math.round(ww/150)){
+        if(data[ ((i + j*ww)*4) + 3] > 150){
+            particles.push(new Particle(i,j));
+        }
+    }
+}
+amount = particles.length;
+
+}
+
+function onMouseClick(){
+radius++;
+if(radius ===5){
+    radius = 0;
+}
+}
+
+function render(a) {
+requestAnimationFrame(render);
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+for (var i = 0; i < amount; i++) {
+    particles[i].render();
+}
+};
+
+copy.addEventListener("keyup", initScene);
+window.addEventListener("resize", initScene);
+window.addEventListener("mousemove", onMouseMove);
+window.addEventListener("touchmove", onTouchMove);
+window.addEventListener("click", onMouseClick);
+window.addEventListener("touchend", onTouchEnd);
+initScene();
+requestAnimationFrame(render);
+
